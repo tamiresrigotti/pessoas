48f03f7dd0b735ff11e86dd2fa805a22e5aaf8fc
tamiresrigoti <tamiresrigoti@gmail.com>
2023-11-10 08:38:28 -0300

    Inicia o projeto

---
+++ src/test/java/com/attornatus/pessoas/PessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attornatus.pessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
