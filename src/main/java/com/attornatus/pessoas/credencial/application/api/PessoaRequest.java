1f82f75bccaf9b68572be7ec0ea89dda1d5c4518
tamiresrigoti <tamiresrigoti@gmail.com>
2023-12-01 18:55:14 -0300

    implementa request e response

--- src/main/java/com/attornatus/pessoas/credencial/application/api/PessoaRequest.java
+++ src/main/java/com/attornatus/pessoas/credencial/application/api/PessoaRequest.java
@@ -1,5 +1,24 @@
 package com.attornatus.pessoas.credencial.application.api;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.attornatus.pessoas.pessoa.domain.Endereco;
+
+import lombok.Value;
+
+@Value
 public class PessoaRequest {
 
+	  @NotBlank
+	    private String nomeCompleto;
+
+	    @NotNull
+	    private LocalDate dataDeNascimento;
+
+	    @NotNull
+	    private Endereco endereco;
 }
+ 
--- src/main/java/com/attornatus/pessoas/credencial/application/api/PessoaResponse.java
+++ src/main/java/com/attornatus/pessoas/credencial/application/api/PessoaResponse.java
@@ -1,5 +1,11 @@
 package com.attornatus.pessoas.credencial.application.api;
 
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
 public class PessoaResponse {
+	private UUID idPessoa; 
 
 }
